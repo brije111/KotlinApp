b6aa3c8f2c7c64c89d423dca3a07d9328d0113a6
BRIJESH <brije111@gmail.com>
2018-10-29 18:53:17 +0530

    Initial commit

---
+++ app/src/main/java/com/android/kotlinapp/MainActivity.kt
@@ -0,0 +1,33 @@
+package com.android.kotlinapp
+
+import android.databinding.DataBindingUtil
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import com.android.kotlinapp.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var binding:ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        //binding.repositoryName.text = "Modern Android Medium Article"
+        /*binding.apply {
+            repositoryName.text = "Medium Android Repository Article"
+            repositoryOwner.text = "Mladen Rakonjac"
+            numberOfStarts.text = "1000 stars"
+
+        }*/
+        var repository = Repository("Medium Android Repository Article",
+            "Mladen Rakonjac", 1000, true)
+        binding.repository = repository
+        binding.executePendingBindings()
+
+        Handler().postDelayed({ repository.repositoryName = "New Name"
+        binding.repository=repository
+        binding.executePendingBindings()
+        }, 2000)
+    }
+}
