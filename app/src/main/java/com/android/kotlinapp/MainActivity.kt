0acca9f4364e7c8bf66e8bd6a04dd8486bb88257
brijesh_mac <brije111@gmail.com>
2018-11-01 08:58:14 +0530

    Adding MVVM pattern

--- app/src/main/java/com/android/kotlinapp/MainActivity.kt
+++ app/src/main/java/com/android/kotlinapp/MainActivity.kt
@@ -1,5 +1,7 @@
 package com.android.kotlinapp
 
+import android.arch.lifecycle.ViewModelProvider
+import android.arch.lifecycle.ViewModelProviders
 import android.databinding.DataBindingUtil
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
@@ -9,10 +11,14 @@ import com.android.kotlinapp.databinding.ActivityMainBinding
 class MainActivity : AppCompatActivity() {
 
     lateinit var binding:ActivityMainBinding
+    //var mainViewModel = MainViewModel()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        //binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        binding.viewModel = ViewModelProviders.of(this).get(MainViewModel::class.java)
+        binding.executePendingBindings()
         //binding.repositoryName.text = "Modern Android Medium Article"
         /*binding.apply {
             repositoryName.text = "Medium Android Repository Article"
@@ -20,7 +26,7 @@ class MainActivity : AppCompatActivity() {
             numberOfStarts.text = "1000 stars"
 
         }*/
-        var repository = Repository("Medium Android Repository Article",
+        /*var repository = Repository("Medium Android Repository Article",
             "Mladen Rakonjac", 1000, true)
         binding.repository = repository
         binding.executePendingBindings()
@@ -28,6 +34,6 @@ class MainActivity : AppCompatActivity() {
         Handler().postDelayed({ repository.repositoryName = "New Name"
         //binding.repository=repository
             //binding.executePendingBindings()
-        }, 2000)
+        }, 2000)*/
     }
 }
--- app/src/main/java/com/android/kotlinapp/MainViewModel.kt
+++ app/src/main/java/com/android/kotlinapp/MainViewModel.kt
@@ -1,7 +1,24 @@
 package com.android.kotlinapp
 
-class MainViewModel{
+import android.arch.lifecycle.ViewModel
+import android.databinding.ObservableField
+
+class MainViewModel : ViewModel(){
     var repoModel:RepoModel = RepoModel();
-    var text : String = ""
-    var isLoading:Boolean = false
+    val text = ObservableField("Old Data")
+    val isLoading = ObservableField(false)
+
+    var onDataReadyCallback = object:RepoModel.OnDataReadyCallback {
+        override fun onDataReady(data: String) {
+            isLoading.set(false);
+            text.set(data)
+        }
+
+
+    }
+
+    fun refresh(){
+        isLoading.set(true)
+        repoModel.refreshData(onDataReadyCallback)
+    }
 }
